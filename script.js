224ab4e35a2a393020f16ee6d52e960cc8ba21d0
sharmasambhav955-cmyk <sharmasambhav955@gmail.com>
2026-05-30 14:42:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", () => {
+
+  const navbar =
+  document.querySelector(".navbar");
+
+  if(window.scrollY > 50){
+
+    navbar.style.background =
+    "rgba(0,0,0,0.7)";
+
+  }
+  else{
+
+    navbar.style.background =
+    "rgba(0,0,0,0.3)";
+
+  }
+
+});
