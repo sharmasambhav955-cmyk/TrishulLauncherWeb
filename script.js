f5c185a4698e5a19de51d66777f0c28e1a5d2a10
notsaiyan <sharmasambhav955@gmail.com>
2026-06-14 22:26:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 window.addEventListener("scroll", () => {
-
-  const navbar =
-  document.querySelector(".navbar");
-
+  const navbar = document.querySelector(".navbar");
   if(window.scrollY > 50){
-
-    navbar.style.background =
-    "rgba(0,0,0,0.7)";
-
+    navbar.style.background = "rgba(0,0,0,0.85)";
+  } else {
+    navbar.style.background = "rgba(0,0,0,0.3)";
   }
-  else{
-
-    navbar.style.background =
-    "rgba(0,0,0,0.3)";
-
-  }
-
 });
