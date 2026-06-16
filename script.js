fc7a8fbf414fb0100a19803a84d3ce9826a3dcea
notsaiyan <sharmasambhav955@gmail.com>
2026-06-16 14:12:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ window.addEventListener("scroll", () => {
     navbar.style.background = "rgba(0,0,0,0.3)";
   }
 });
+
+// Mobile menu toggle open/close
+const mobileMenu = document.getElementById("mobile-menu");
+const navMenu = document.getElementById("nav-menu");
+
+if (mobileMenu && navMenu) {
+  mobileMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    mobileMenu.classList.toggle("is-active"); // Animation effects ke liye
+  });
+}
