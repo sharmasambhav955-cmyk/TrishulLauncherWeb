bded1239851ba3187104bac5d342498e3e6e9cfe
notsaiyan <sharmasambhav955@gmail.com>
2026-07-03 21:48:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-window.addEventListener("scroll", () => {
-  const navbar = document.querySelector(".navbar");
-  if(window.scrollY > 50){
-    navbar.style.background = "rgba(0,0,0,0.85)";
-  } else {
-    navbar.style.background = "rgba(0,0,0,0.3)";
-  }
-});
-
-// Mobile menu toggle open/close
+// Toggle hamburger layout
 const mobileMenu = document.getElementById("mobile-menu");
 const navMenu = document.getElementById("nav-menu");
 
 if (mobileMenu && navMenu) {
   mobileMenu.addEventListener("click", () => {
     navMenu.classList.toggle("active");
-    mobileMenu.classList.toggle("is-active"); // Animation effects ke liye
   });
 }
